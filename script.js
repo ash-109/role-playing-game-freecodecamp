12d6ea8da51573bce066b046dc3298fd43df9b7a
Ashvin Antyah <a.antyah@gmail.com>
2024-04-08 21:57:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let xp = 0;
-let health =100;
+let health = 100;
 let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-let inventory =[ "stick"];
+let inventory = ["stick"];
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
@@ -18,15 +18,53 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-function goStore(){
-    console.log("Going to store.");
+function goTown() {
+  update(locations[0])
 }
-function goCave(){
-    console.log("Going to cave.");
+
+function goStore() {
+ 
 }
+
+function goCave() {
+  console.log("Going to cave.");
+}
+
 function fightDragon() {
-    console.log("Fighting dragon.");
+  console.log("Fighting dragon.");
+}
+function buyHealth() {}
+function buyWeapon() {}
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+
+  text.innerText =
+  "You are in the town square. You see a sign that says \"Store\".";
 }
 
-// initialize buttons
 
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  }
+];
